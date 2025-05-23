96c4b4a8a64945e75ec7827d53beef2641f71086
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2025-05-23 16:11:20 -0400

    Remove title from Dashboard page

--- frontend/src/components/DashboardPage.vue
+++ frontend/src/components/DashboardPage.vue
@@ -5,7 +5,6 @@ withDefaults(
   defineProps<{
     breadcrumbs?: VBreadcrumbs['$props']['items']
     fluid?: boolean
-    title?: string
   }>(),
   {
     fluid: true,
@@ -14,8 +13,12 @@ withDefaults(
 </script>
 <template>
   <VContainer :fluid="fluid" class="h-100 d-flex flex-column py-0 h-100">
-    <VBreadcrumbs :items="breadcrumbs" density="comfortable" class="pa-0" v-if="breadcrumbs" />
-    <h1 v-if="title" v-text="title" class="text-h5 font-bold ma-1" />
+    <VBreadcrumbs
+      :items="breadcrumbs"
+      density="comfortable"
+      class="pa-0 flex-wrap"
+      v-if="breadcrumbs"
+    />
     <slot />
   </VContainer>
 </template>
