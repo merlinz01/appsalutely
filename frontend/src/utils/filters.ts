96cdd15e9591d26b4a71cf33d3afbb6256ae8c1d
merlinz01 <158784988+merlinz01@users.noreply.github.com>
2025-05-30 10:58:44 -0400

    Change usage of any to unknown in filter type

--- frontend/src/utils/filters.ts
+++ frontend/src/utils/filters.ts
@@ -1,4 +1,4 @@
-export type Filter = [string, string, any]
+export type Filter = [string, string, unknown]
 export type Filters = Filter[]
 export function asParams(filters: Filters): { filter?: string } {
   if (!filters || filters.length === 0) {
